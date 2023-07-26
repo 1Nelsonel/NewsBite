fda1a29af34006d54a05c0b8ea43ec16ff8c37a2
Nelson Mokoro <nelsonmokoro37@gmail.com>
2023-07-26 15:13:15 +0300

    success

---
+++ models/post.model.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/gosimple/slug"
+	"gorm.io/gorm"
+)
+
+type Post struct {
+	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id,omitempty"`
+	Slug      string    `gorm:"uniqueIndex;not null" json:"slug,omitempty"`
+	Title     string    `gorm:"uniqueIndex;not null" json:"title,omitempty"`
+	Content   string    `gorm:"not null" json:"content,omitempty"`
+	Image     string    `gorm:"not null" json:"image,omitempty"`
+	User      uuid.UUID `gorm:"not null" json:"user,omitempty"`
+	CreatedAt time.Time `gorm:"not null" json:"created_at,omitempty"`
+	UpdatedAt time.Time `gorm:"not null" json:"updated_at,omitempty"`
+}
+
+type CreatePostRequest struct {
+	Title     string    `json:"title"  binding:"required"`
+	Content   string    `json:"content" binding:"required"`
+	Image     string    `json:"image" binding:"required"`
+	User      string    `json:"user,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
+}
+
+type UpdatePost struct {
+	Title     string    `json:"title,omitempty"`
+	Content   string    `json:"content,omitempty"`
+	Image     string    `json:"image,omitempty"`
+	User      string    `json:"user,omitempty"`
+	CreateAt  time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
+}
+
+// BeforeCreate hook to automatically set the slug before creating a new Post
+func (post *Post) BeforeCreate(tx *gorm.DB) (err error) {
+	post.Slug = slug.Make(post.Title)
+	return
+}
